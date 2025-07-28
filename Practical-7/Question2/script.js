42076a68637db78279eb55051a94f60076df65cc
Nishit079 <pnishit004@gmail.com>
2025-07-28 21:22:23 +0530

    Create script.js

---
+++ Practical-7/Question2/script.js
@@ -0,0 +1,15 @@
+// Run with Node Js directly from the terminal for that you have to first install Node Js in your system. and then install coderunner extension in vs code. and then run the code by pressing ctrl+alt+n.
+
+
+var myInteger = 42;
+
+let myFloat = 3.14;
+
+const myChar = 'A';
+
+let myString = "Hello, World!";
+
+console.log("Integer:", myInteger);
+console.log("Floating-point number:", myFloat);
+console.log("Character:", myChar);
+console.log("String:", myString);
